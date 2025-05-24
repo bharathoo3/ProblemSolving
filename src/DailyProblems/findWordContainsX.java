ccb66aaa93da245f46a60eba5ee1dc6cdc5ef1d7
rambh <rambharath979@gmail.com>
2025-05-24 20:58:41 +0530

    init commit

---
+++ src/DailyProblems/findWordContainsX.java
@@ -0,0 +1,19 @@
+package DailyProblems;
+import java.util.*;
+public class findWordContainsX {
+    public static void main(String[] args) {
+        String[] str={"leet","code","solution"};
+        char x='e';
+        System.out.println(findWordContaing(str,x));
+    }
+    public static List<Integer> findWordContaing(String[] str,char x)
+    {
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<str.length;i++)
+        {
+            if(str[i].contains(String.valueOf(x)))
+            list.add(i);
+        }
+        return list;
+    }
+}
