277c3f2820818ed271ed59241e70bfc2ee48e58b
rambh <rambharath979@gmail.com>
2025-06-01 11:44:01 +0530

    init commit

--- src/leSuccess/dayNine/hw/find_a.java
+++ src/leSuccess/dayNine/hw/find_a.java
@@ -5,6 +5,8 @@ public class find_a {
         String s="aba";
         int n=10;
         System.out.println(findA(s,n));
+        // optimal way
+        System.out.println(optimalWayForFindA(s,n));
     }
     static int findA(String s,int n)
     {
@@ -19,5 +21,21 @@ public class find_a {
             freq[str.charAt(i)-'a']++;
         }
         return freq[0];
+    }
+    static  int optimalWayForFindA(String s,int n)
+    {
+        int count=0;
+        for(char c:s.toCharArray())
+        {
+            if(c=='a')count++;
+        }
+        int fullReapeat=n/s.length();
+        int remingletter=n%s.length();
+        count*=fullReapeat;
+        for (int i=0;i<remingletter;i++)
+        {
+            if(s.charAt(i)=='a')count++;
+        }
+        return count;
     }
 }
